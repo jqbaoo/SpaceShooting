691b06d159d226549eba0fbf832bfc6fe177a47a
jqbao <godofwar@126.com>
2021-01-16 13:47:04 +0800

    test

---
+++ SpaceShooting/Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("测试！！！");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
